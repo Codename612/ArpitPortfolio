3c7ee865f6a63815717378763b410fbe34d6ed1e
Arpit Singh <arp.s0612@gmail.com>
2026-01-02 21:09:02 -0500

    Demo changes+Made Resume display modular

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -16,27 +16,44 @@ class Resume extends Component {
     if(this.props.data){
       var skillmessage = this.props.data.skillmessage;
 
-      var education = this.props.data.education.map(function(education){
-        return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="info">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
-      })
+      var education = (this.props.data.education && Array.isArray(this.props.data.education)) ? this.props.data.education.map(function(education){
+        return (
+          <div key={education.school}>
+            <h3>{education.school}</h3>
+            <p className="info">{education.degree} <span>&bull;</span><em className="info">{education.graduated}</em></p>
+            <p>{education.description}</p>
+          </div>
+        );
+      }) : null;
 
-      var work = this.props.data.work.map(function(work){
-        return <div key={work.company}><h3>{work.company}</h3>
+      var work = (this.props.data.work && Array.isArray(this.props.data.work)) ? this.props.data.work.map(function(work){
+        // Normalize description to array, filter out empty strings and guard against undefined/null
+        const descriptions = work.description
+          ? (Array.isArray(work.description)
+              ? work.description.filter(item => item && item.toString().trim())
+              : (typeof work.description === 'string' && work.description.trim() ? [work.description.trim()] : []))
+          : [];
+        return (
+          <div key={work.company}>
+            <h3>{work.company}</h3>
             <p className="info">{work.title}<span>&bull;</span> <em className="info">{work.years}</em></p>
-            {work.description.map((item, index) => (
-              <li>{item}</li>
-            ))}
-        </div>
-      })
+            {descriptions.length > 0 && (
+              <ul className="work-description">
+                {descriptions.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+            )}
+          </div>
+        );
+      }) : null;
 
-      var skills = this.props.data.skills.map(function(skills){
+      var skills = (this.props.data.skills && Array.isArray(this.props.data.skills)) ? this.props.data.skills.map(function(skills){
         return <div key={skills.name}>
             <p className="info"><b>{skills.name}</b></p>
             <p>{skills.description}</p>
         </div>
-      })
+      }) : null;
     }
 
     return (
